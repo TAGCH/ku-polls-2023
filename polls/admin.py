30bab288ecda1436d0babdc96e0f6ab878134da4
TAGCH <pisit.au@ku.th>
2023-09-15 23:13:28 +0700

    Create new json

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date', 'end_date'],
+                              'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date',
+                    'was_published_recently', 'end_date')
+
+
 admin.site.register(Question)
 admin.site.register(Choice)
